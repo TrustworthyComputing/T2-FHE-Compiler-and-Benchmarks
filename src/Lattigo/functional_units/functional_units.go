f278df11b26fe901732350d358954af727a40833
Dimitris Mouris <jimouris@udel.edu>
2022-01-10 13:40:00 -0500

    Minor changes in format

--- src/Lattigo/functional_units/functional_units.go
+++ src/Lattigo/functional_units/functional_units.go
@@ -1,8 +1,8 @@
 package functional_units
 
 import (
-	"math"
 	"github.com/ldsec/lattigo/v2/bfv"
+	"math"
 )
 
 var encryptorPk_ *bfv.Encryptor
@@ -12,9 +12,9 @@ var params_ *bfv.Parameters
 var ptxt_mod_ int
 var slots_ int
 
-func FunitsInit(encryptorPk *bfv.Encryptor, encoder *bfv.Encoder, 
-							 	evaluator *bfv.Evaluator, params *bfv.Parameters,
-								ptxt_mod, slots int) {
+func FunitsInit(encryptorPk *bfv.Encryptor, encoder *bfv.Encoder,
+	evaluator *bfv.Evaluator, params *bfv.Parameters,
+	ptxt_mod, slots int) {
 	encryptorPk_ = encryptorPk
 	encoder_ = encoder
 	evaluator_ = evaluator
@@ -23,24 +23,24 @@ func FunitsInit(encryptorPk *bfv.Encryptor, encoder *bfv.Encoder,
 	slots_ = slots
 }
 
-func EqPlain(ctxt *bfv.Ciphertext, ptxt *bfv.Plaintext) (*bfv.Ciphertext) {
+func EqPlain(ctxt *bfv.Ciphertext, ptxt *bfv.Plaintext) *bfv.Ciphertext {
 	ctxt2 := (*encryptorPk_).EncryptNew(ptxt)
 	return Eq(ctxt, ctxt2)
 }
 
-func Eq(c1, c2 *bfv.Ciphertext) (*bfv.Ciphertext) {
+func Eq(c1, c2 *bfv.Ciphertext) *bfv.Ciphertext {
 	num_squares := int(math.Log2(float64(ptxt_mod_ - 1)))
 	tmp_ := c1.CopyNew()
 	(*evaluator_).Sub(tmp_, c2, tmp_)
 	tmp2_ := tmp_.CopyNew()
 	for i := 0; i < num_squares; i++ { // square
-		receiver := bfv.NewCiphertext(*params_, tmp2_.Degree() * 2)
+		receiver := bfv.NewCiphertext(*params_, tmp2_.Degree()*2)
 		(*evaluator_).Mul(tmp2_, tmp2_, receiver)
-		tmp2_= (*evaluator_).RelinearizeNew(receiver)
+		tmp2_ = (*evaluator_).RelinearizeNew(receiver)
 	}
 	remaining := int((ptxt_mod_ - 1) - int(math.Pow(2.0, float64(num_squares))))
 	for i := 0; i < remaining; i++ { // mult
-		receiver := bfv.NewCiphertext(*params_, tmp2_.Degree() + tmp_.Degree())
+		receiver := bfv.NewCiphertext(*params_, tmp2_.Degree()+tmp_.Degree())
 		(*evaluator_).Mul(tmp2_, tmp_, receiver)
 		tmp2_ = (*evaluator_).RelinearizeNew(receiver)
 	}
@@ -56,17 +56,17 @@ func Eq(c1, c2 *bfv.Ciphertext) (*bfv.Ciphertext) {
 	return tmp2_
 }
 
-func LtPlainRight(ctxt *bfv.Ciphertext, ptxt *bfv.Plaintext) (*bfv.Ciphertext) {
+func LtPlainRight(ctxt *bfv.Ciphertext, ptxt *bfv.Plaintext) *bfv.Ciphertext {
 	ctxt2 := (*encryptorPk_).EncryptNew(ptxt)
 	return Lt(ctxt, ctxt2)
 }
 
-func LtPlainLeft(ptxt *bfv.Plaintext, ctxt *bfv.Ciphertext) (*bfv.Ciphertext) {
+func LtPlainLeft(ptxt *bfv.Plaintext, ctxt *bfv.Ciphertext) *bfv.Ciphertext {
 	ctxt1 := (*encryptorPk_).EncryptNew(ptxt)
 	return Lt(ctxt1, ctxt)
 }
 
-func Lt(c1, c2 *bfv.Ciphertext) (*bfv.Ciphertext) {
+func Lt(c1, c2 *bfv.Ciphertext) *bfv.Ciphertext {
 	num_squares := int(math.Log2(float64(ptxt_mod_ - 1)))
 	ptxt := bfv.NewPlaintext(*params_)
 	arr := make([]uint64, slots_)
@@ -90,18 +90,17 @@ func Lt(c1, c2 *bfv.Ciphertext) (*bfv.Ciphertext) {
 		tmp2_ := tmp_.CopyNew()
 
 		for j := 0; j < num_squares; j++ { // square
-			receiver := bfv.NewCiphertext(*params_, tmp2_.Degree() * 2)
+			receiver := bfv.NewCiphertext(*params_, tmp2_.Degree()*2)
 			(*evaluator_).Mul(tmp2_, tmp2_, receiver)
-			tmp2_= (*evaluator_).RelinearizeNew(receiver)
+			tmp2_ = (*evaluator_).RelinearizeNew(receiver)
 		}
 		remaining := int((ptxt_mod_ - 1) - int(math.Pow(2.0, float64(num_squares))))
 		for j := 0; j < remaining; j++ { // mult
-			receiver := bfv.NewCiphertext(*params_, tmp2_.Degree() + tmp_.Degree())
+			receiver := bfv.NewCiphertext(*params_, tmp2_.Degree()+tmp_.Degree())
 			(*evaluator_).Mul(tmp2_, tmp_, receiver)
 			tmp2_ = (*evaluator_).RelinearizeNew(receiver)
 		}
 		tmp_ = tmp2_.CopyNew()
-		// tmp_ = tmp2_
 		(*evaluator_).Neg(tmp_, tmp_)
 		for j := 0; j < slots_; j++ {
 			arr[j] = 1
@@ -114,23 +113,23 @@ func Lt(c1, c2 *bfv.Ciphertext) (*bfv.Ciphertext) {
 	return result_
 }
 
-func LeqPlainRight(ctxt *bfv.Ciphertext, ptxt *bfv.Plaintext) (*bfv.Ciphertext) {
+func LeqPlainRight(ctxt *bfv.Ciphertext, ptxt *bfv.Plaintext) *bfv.Ciphertext {
 	ctxt2 := (*encryptorPk_).EncryptNew(ptxt)
 	return Leq(ctxt, ctxt2)
 }
 
-func LeqPlainLeft(ptxt *bfv.Plaintext, ctxt *bfv.Ciphertext) (*bfv.Ciphertext) {
+func LeqPlainLeft(ptxt *bfv.Plaintext, ctxt *bfv.Ciphertext) *bfv.Ciphertext {
 	ctxt1 := (*encryptorPk_).EncryptNew(ptxt)
 	return Leq(ctxt1, ctxt)
 }
 
-func Leq(c1, c2 *bfv.Ciphertext) (*bfv.Ciphertext) {
+func Leq(c1, c2 *bfv.Ciphertext) *bfv.Ciphertext {
 	less_ := Lt(c1, c2)
 	equal_ := Eq(c1, c2)
 	tmp_ := less_.CopyNew()
-  receiver := bfv.NewCiphertext(*params_, tmp_.Degree()+equal_.Degree())
+	receiver := bfv.NewCiphertext(*params_, tmp_.Degree()+equal_.Degree())
 	(*evaluator_).Mul(tmp_, equal_, receiver)
-  tmp_ = (*evaluator_).RelinearizeNew(receiver)
+	tmp_ = (*evaluator_).RelinearizeNew(receiver)
 	result_ := less_.CopyNew()
 	(*evaluator_).Add(result_, equal_, result_)
 	(*evaluator_).Sub(result_, tmp_, result_)
