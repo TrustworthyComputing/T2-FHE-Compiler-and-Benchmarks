9c68cd8bea9b8a9c24e448b76600af1ebc6c6acb
Chaz Gouert <cgouert@udel.edu>
2022-01-16 19:32:51 -0500

    SEAL: Update binary tests for batching

--- src/SEAL/functional_units/functional_units_test_binary.cpp
+++ src/SEAL/functional_units/functional_units_test_binary.cpp
@@ -5,13 +5,11 @@ using namespace std;
 using namespace seal;
 
 int main(void) {
-  size_t poly_modulus_degree = 16384;
-  size_t plaintext_modulus = 2;
-  size_t word_sz = 8;
+  size_t poly_modulus_degree = 32768;
   EncryptionParameters parms(scheme_type::bfv);
   parms.set_poly_modulus_degree(poly_modulus_degree);
   parms.set_coeff_modulus(CoeffModulus::BFVDefault(poly_modulus_degree));
-  parms.set_plain_modulus(plaintext_modulus);
+  parms.set_plain_modulus(PlainModulus::Batching(poly_modulus_degree, 20));
   SEALContext context(parms);
   KeyGenerator keygen(context);
   SecretKey secret_key = keygen.secret_key();
@@ -22,18 +20,23 @@ int main(void) {
   Encryptor encryptor(context, public_key);
   Evaluator evaluator(context);
   Decryptor decryptor(context, secret_key);
+  BatchEncoder batch_encoder(context);
+  size_t slots = poly_modulus_degree/2;
+  size_t word_sz = 8;
   vector<Plaintext> bit_input(word_sz);
+  vector<uint64_t> tmp(1);
   vector<Ciphertext> ct1_(word_sz);
   vector<Ciphertext> ct2_(word_sz);
   for (int i = 0; i < word_sz; i++) {
-    bit_input[i] = uint64_to_hex_string(i % 2);
+    tmp[0] = i % 2;
+    batch_encoder.encode(tmp, bit_input[i]);
     encryptor.encrypt(bit_input[i], ct1_[i]);
     ct2_[i] = ct1_[i];
   }
 
   cout << "EQUALITY TEST" << endl;
-  vector<Ciphertext> res_ = eq_bin(evaluator, encryptor, relin_keys, ct1_, ct2_,
-                                   word_sz);
+  vector<Ciphertext> res_ = eq_bin(evaluator, encryptor, batch_encoder, relin_keys, ct1_, ct2_,
+                                   word_sz, slots);
   
   cout << "Answer should be 1 and then all 0's: ";
   for (int i = 0; i < word_sz; i++) {
@@ -43,12 +46,13 @@ int main(void) {
   cout << endl;
 
   for (int i = 0; i < word_sz; i++) {
-    bit_input[i] = uint64_to_hex_string((i+1)%2);
+    tmp[0] = (i+1)%2;
+    batch_encoder.encode(tmp, bit_input[i]);
     encryptor.encrypt(bit_input[i], ct2_[i]);
   }
 
-  res_ = eq_bin(evaluator, encryptor, relin_keys, ct1_, ct2_,
-                                   word_sz);
+  res_ = eq_bin(evaluator, encryptor, batch_encoder, relin_keys, ct1_, ct2_,
+                                   word_sz, slots);
   
   cout << "Answer should be all 0's: ";
   for (int i = 0; i < word_sz; i++) {
