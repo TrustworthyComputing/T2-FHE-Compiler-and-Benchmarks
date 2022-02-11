cfb44f5998aa626c226e91241e07e2ceb3efcdfa
Chaz Gouert <cgouert@udel.edu>
2022-02-10 19:47:18 -0500

    WIP #15: Exclude ternary test for PALISADE due to library issues
    Co-authored-by: Dimitris Mouris <jimouris@udel.edu>

--- .circleci/test_PALISADE.sh
+++ .circleci/test_PALISADE.sh
@@ -24,13 +24,15 @@ make
 diff <(head -n -1 ../test/resources/tests/batching_PALISADE.log | awk '{$1=$1};1' | cut -d ' ' -f 3-) ../test/resources/tests/batching.res
 cd ../..
 
-java -jar target/terminator-compiler-1.0.jar src/test/resources/tests/ternary.t2 --PALISADE --config src/main/java/org/twc/terminator/t2dsl_compiler/configs/palisade-bgv-int-20-levels.config
-cp ./src/test/resources/tests/ternary.cpp ./src/PALISADE/compiled/test.cpp
-cd ./src/PALISADE
-make
-./bin/test.out > ../test/resources/tests/ternary_PALISADE.log
-diff <(head -n -1 ../test/resources/tests/ternary_PALISADE.log | awk '{$1=$1};1' | cut -d ' ' -f 3-) ../test/resources/tests/ternary.res
-cd ../..
+### Exclude since it randomly throws floating-point exception with insecure 
+### parameters and it takes forever otherwise
+# java -jar target/terminator-compiler-1.0.jar src/test/resources/tests/ternary.t2 --PALISADE --config src/main/java/org/twc/terminator/t2dsl_compiler/configs/palisade-bgv-int-20-levels.config
+# cp ./src/test/resources/tests/ternary.cpp ./src/PALISADE/compiled/test.cpp
+# cd ./src/PALISADE
+# make
+# ./bin/test.out > ../test/resources/tests/ternary_PALISADE.log
+# diff <(head -n -1 ../test/resources/tests/ternary_PALISADE.log | awk '{$1=$1};1' | cut -d ' ' -f 3-) ../test/resources/tests/ternary.res
+# cd ../..
 
 java -jar target/terminator-compiler-1.0.jar src/test/resources/tests/arrays.t2 --PALISADE --config src/main/java/org/twc/terminator/t2dsl_compiler/configs/palisade-bgv-int-2-levels.config
 cp ./src/test/resources/tests/arrays.cpp ./src/PALISADE/compiled/test.cpp
@@ -57,13 +59,15 @@ make
 diff <(head -n -1 ../test/resources/tests/batching_w5_PALISADE.log | awk '{$1=$1};1' | cut -d ' ' -f 3-) ../test/resources/tests/batching_w5.res
 cd ../..
 
-java -jar target/terminator-compiler-1.0.jar src/test/resources/tests/ternary.t2 --PALISADE --w 6 --config src/main/java/org/twc/terminator/t2dsl_compiler/configs/palisade-bgv-bin-10-levels.config
-cp ./src/test/resources/tests/ternary.cpp ./src/PALISADE/compiled/test.cpp
-cd ./src/PALISADE
-make
-./bin/test.out > ../test/resources/tests/ternary_w6_PALISADE.log
-diff <(head -n -1 ../test/resources/tests/ternary_w6_PALISADE.log | awk '{$1=$1};1' | cut -d ' ' -f 3-) ../test/resources/tests/ternary_w6.res
-cd ../..
+### Exclude since it randomly throws floating-point exception with insecure 
+### parameters and it takes forever otherwise
+# java -jar target/terminator-compiler-1.0.jar src/test/resources/tests/ternary.t2 --PALISADE --w 6 --config src/main/java/org/twc/terminator/t2dsl_compiler/configs/palisade-bgv-bin-10-levels.config
+# cp ./src/test/resources/tests/ternary.cpp ./src/PALISADE/compiled/test.cpp
+# cd ./src/PALISADE
+# make
+# ./bin/test.out > ../test/resources/tests/ternary_w6_PALISADE.log
+# diff <(head -n -1 ../test/resources/tests/ternary_w6_PALISADE.log | awk '{$1=$1};1' | cut -d ' ' -f 3-) ../test/resources/tests/ternary_w6.res
+# cd ../..
 
 java -jar target/terminator-compiler-1.0.jar src/test/resources/tests/arrays.t2 --PALISADE --w 5 --config src/main/java/org/twc/terminator/t2dsl_compiler/configs/palisade-bgv-bin-10-levels.config
 cp ./src/test/resources/tests/arrays.cpp ./src/PALISADE/compiled/test.cpp
