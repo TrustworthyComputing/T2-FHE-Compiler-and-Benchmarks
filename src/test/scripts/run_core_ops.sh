9336199de14c7d146ce821c477f6074ab8156356
Chaz Gouert <cgouert@udel.edu>
2022-02-19 17:08:20 -0500

    Generate more configs for equality

--- src/test/scripts/run_core_ops.sh
+++ src/test/scripts/run_core_ops.sh
@@ -41,17 +41,17 @@ make
 cd ../..
 
 java -jar target/terminator-compiler-1.0.jar \
-  src/test/resources/tests/core_operations/comp.t2 --HELIB \
-  --config src/test/resources/tests/core_operations/configs/helib-64k-128-int.config
-cp ./src/test/resources/tests/core_operations/comp.cpp ./src/HElib/compiled/test.cpp
+  src/test/resources/tests/core_operations/eq.t2 --HELIB \
+  --config src/test/resources/tests/core_operations/configs/helib-32k-128-int.config
+cp ./src/test/resources/tests/core_operations/eq.cpp ./src/HElib/compiled/test.cpp
 cd ./src/HElib
 make
-./bin/test.out > ../test/resources/tests/core_operations/logs/comp_int_HElib.log
+./bin/test.out > ../test/resources/tests/core_operations/logs/eq_int_HElib.log
 cd ../..
 
 java -jar target/terminator-compiler-1.0.jar \
   src/test/resources/tests/core_operations/eq.t2 --SEAL \
-  --config src/test/resources/tests/core_operations/configs/seal-16k-128-int.config
+  --config src/test/resources/tests/core_operations/configs/seal-32k-128-int.config
 cp ./src/test/resources/tests/core_operations/eq.cpp ./src/SEAL/compiled/test.cpp
 cd ./src/SEAL
 make
@@ -60,7 +60,7 @@ cd ../..
 
 java -jar target/terminator-compiler-1.0.jar \
   src/test/resources/tests/core_operations/eq.t2 --PALISADE \
-  --config src/test/resources/tests/core_operations/configs/palisade-16k-128-int.config
+  --config src/test/resources/tests/core_operations/configs/palisade-32k-128-int.config
 cp ./src/test/resources/tests/core_operations/eq.cpp ./src/PALISADE/compiled/test.cpp
 cd ./src/PALISADE
 make
@@ -69,11 +69,20 @@ cd ../..
 
 java -jar target/terminator-compiler-1.0.jar \
   src/test/resources/tests/core_operations/eq.t2 --LATTIGO \
-  --config src/test/resources/tests/core_operations/configs/lattigo-16k-128-int.config
+  --config src/test/resources/tests/core_operations/configs/lattigo-32k-128-int.config
 cp ./src/test/resources/tests/core_operations/eq.go ./src/Lattigo/compiled/test.go
 cd ./src/Lattigo
 make
 ./bin/test.out > ../test/resources/tests/core_operations/logs/eq_int_Lattigo.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/comp.t2 --HELIB \
+  --config src/test/resources/tests/core_operations/configs/helib-64k-128-int.config
+cp ./src/test/resources/tests/core_operations/comp.cpp ./src/HElib/compiled/test.cpp
+cd ./src/HElib
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/comp_int_HElib.log
 cd ../..
 
 # Binary Domain
@@ -120,6 +129,50 @@ cp ./src/test/resources/tests/core_operations/add_mult.cpp ./src/TFHE/compiled/t
 cd ./src/TFHE
 make
 ./bin/test.out > ../test/resources/tests/core_operations/logs/add_mult_bin_TFHE.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/eq.t2 --HELIB --w 8 \
+  --config src/test/resources/tests/core_operations/configs/helib-32k-128-bin.config
+cp ./src/test/resources/tests/core_operations/eq.cpp ./src/HElib/compiled/test.cpp
+cd ./src/HElib
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/eq_bin_HElib.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/eq.t2 --SEAL --w 8 \
+  --config src/test/resources/tests/core_operations/configs/seal-32k-128-bin.config
+cp ./src/test/resources/tests/core_operations/eq.cpp ./src/SEAL/compiled/test.cpp
+cd ./src/SEAL
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/eq_bin_SEAL.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/eq.t2 --PALISADE --w 8 \
+  --config src/test/resources/tests/core_operations/configs/palisade-32k-128-bin.config
+cp ./src/test/resources/tests/core_operations/eq.cpp ./src/PALISADE/compiled/test.cpp
+cd ./src/PALISADE
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/eq_bin_PALISADE.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/eq.t2 --LATTIGO --w 8 \
+  --config src/test/resources/tests/core_operations/configs/lattigo-32k-128-bin.config
+cp ./src/test/resources/tests/core_operations/eq.go ./src/Lattigo/compiled/test.go
+cd ./src/Lattigo
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/eq_bin_Lattigo.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/eq.t2 --TFHE --w 8
+cp ./src/test/resources/tests/core_operations/eq.cpp ./src/TFHE/compiled/test.cpp
+cd ./src/TFHE
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/eq_bin_TFHE.log
 cd ../..
 
 java -jar target/terminator-compiler-1.0.jar \
