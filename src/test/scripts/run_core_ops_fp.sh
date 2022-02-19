deae222dca8389c10e28c891716301835a59fece
Chaz Gouert <cgouert@udel.edu>
2022-02-19 18:09:04 -0500

    Small bug fix in config and optimize PALISADE parameters

---
+++ src/test/scripts/run_core_ops_fp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -exo pipefail
+
+# Floating Point Domain
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/add_mult_fp.t2 --HELIB \
+  --config src/test/resources/tests/core_operations/configs/helib-16k-128-fp.config
+cp ./src/test/resources/tests/core_operations/add_mult_fp.cpp ./src/HElib/compiled/test.cpp
+cd ./src/HElib
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/add_mult_fp_HElib.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/add_mult_fp.t2 --SEAL \
+  --config src/test/resources/tests/core_operations/configs/seal-16k-128-fp.config
+cp ./src/test/resources/tests/core_operations/add_mult_fp.cpp ./src/SEAL/compiled/test.cpp
+cd ./src/SEAL
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/add_mult_fp_SEAL.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/add_mult_fp.t2 --PALISADE \
+  --config src/test/resources/tests/core_operations/configs/palisade-16k-128-fp.config
+cp ./src/test/resources/tests/core_operations/add_mult_fp.cpp ./src/PALISADE/compiled/test.cpp
+cd ./src/PALISADE
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/add_mult_fp_PALISADE.log
+cd ../..
+
+java -jar target/terminator-compiler-1.0.jar \
+  src/test/resources/tests/core_operations/add_mult_fp.t2 --LATTIGO \
+  --config src/test/resources/tests/core_operations/configs/lattigo-16k-128-fp.config
+cp ./src/test/resources/tests/core_operations/add_mult_fp.go ./src/Lattigo/compiled/test.go
+cd ./src/Lattigo
+make
+./bin/test.out > ../test/resources/tests/core_operations/logs/add_mult_fp_Lattigo.log
+cd ../..
