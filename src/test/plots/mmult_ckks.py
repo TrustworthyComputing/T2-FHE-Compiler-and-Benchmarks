4aaa0b05ca6318f815372226a1f835add7715158
Dimitris Mouris <jimouris@udel.edu>
2022-02-24 18:43:58 -0500

    Minor update in mmult for CKKS

--- src/test/plots/mmult_ckks.py
+++ src/test/plots/mmult_ckks.py
@@ -13,27 +13,23 @@ data = {
   'helib': 10735,
   'lattigo': 146.342248,
   'palisade': 271,
-  'seal': 170,
-  'tfhe': 10 },
+  'seal': 170 },
 '8x8 x 8x8' : {
   'helib': 15438,
   'lattigo': 1156.690684,
   'palisade': 2315,
-  'seal': 1364,
-  'tfhe': 10 },
+  'seal': 1364 },
 '16x16 x 16x16' : {
   'helib': 122990,
   'lattigo': 9182.076787,
   'palisade': 18515,
-  'seal': 10735,
-  'tfhe': 10 }
+  'seal': 10735 }
 }
 
 helib = []
 lattigo = []
 palisade = []
 seal = []
-tfhe = []
 
 x_axis_label = []
 for k,val in data.items():
@@ -42,25 +38,22 @@ for k,val in data.items():
   lattigo.append(val['lattigo'] / 1000)
   palisade.append(val['palisade'] / 1000)
   seal.append(val['seal'] / 1000)
-  tfhe.append(val['tfhe'] / 1000)
 
 N = len(palisade)
 index = np.arange(N) # the x locations for the groups
-width = 0.16 # the width of the bars
+width = 0.2 # the width of the bars
 
 fig, ax = plt.subplots(figsize=pltsize)
 ax.margins(0.02, 0.02)
 
-rects1 = ax.bar(index - 3*width/2 - 2*width/8, helib, width,
+rects1 = ax.bar(index - width - 2*width/8 + width/16, helib, width,
                 color='xkcd:light salmon', hatch='//', edgecolor='black', linewidth=1)
-rects2 = ax.bar(index - width/2 - width/8, lattigo, width,
+rects2 = ax.bar(index - width/8 + width/16, lattigo, width,
                 color='#ffddbf', hatch='xx', edgecolor='black', linewidth=1)
-rects3 = ax.bar(index + width/2, palisade, width,
+rects3 = ax.bar(index + width + width/16, palisade, width,
                 color='xkcd:ecru', hatch='--', edgecolor='black', linewidth=1)
-rects4 = ax.bar(index + 3*width/2 + width/8, seal, width,
+rects4 = ax.bar(index + 2*width + width/8 + width/16, seal, width,
                 color='xkcd:very light green', hatch='..', edgecolor='black', linewidth=1)
-rects5 = ax.bar(index + 5*width/2 + 2*width/8, tfhe, width,
-                color='xkcd:very light blue', hatch='\\\\', edgecolor='black', linewidth=1)
 
 ax.set_yscale('log')
 ax.set_ylim([0.1, 1000])
@@ -68,9 +61,9 @@ ax.set_ylabel("Time (sec.)")
 ax.set_xlabel("Matrices Size")
 ax.set_xticks(index + width / 2)
 ax.set_xticklabels(x_axis_label)
-ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]),
-          ("HElib", "Lattigo", "PALISADE", "SEAL", "TFHE"),
-          fontsize=9, ncol=3, loc='upper left')
+ax.legend((rects1[0], rects2[0], rects3[0], rects4[0]),
+          ("HElib", "Lattigo", "PALISADE", "SEAL"),
+          fontsize=9, ncol=2, loc='upper left')
 
 def autolabel_above(rects):
   for rect in rects:
@@ -89,7 +82,6 @@ autolabel_above(rects1)
 autolabel_above(rects2)
 autolabel_above(rects3)
 autolabel_above(rects4)
-autolabel_above(rects5)
 
 # plt.show()
 
